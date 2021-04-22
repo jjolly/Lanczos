c3024d51cd271733affe7a6cf5b507d18a57c860
John L. Jolly <u1265505@utah.edu>
2021-04-22 13:55:52 -0600

    [lanczos]: Eliminate the temporary pointers for easier analysis

--- src/lanczos.c
+++ src/lanczos.c
@@ -7,70 +7,60 @@ void kernel(int k, double *v, SparseMatrix * A, double *arrt)
 {
   unsigned int i, j, l, m;
   double sum;
-  double *y, *x;
+  double *y;
   sum = 0.0;
   for (i = 0; i < A->nrows; i++) {
     sum += v[i] * v[i];
   }
   sum = 1 / sqrt(sum);
   for (i = 0; i < A->nrows; i++) {
-    // WRITE to x[i]
+    // WRITE to v[i]
     v[i] *= sum;
   }
   for (j = 0; j < k; j++) {
-    y = v + A->nrows * (j + 1);
-    x = v + A->nrows * j;
     sum = 0.0;
     l = 0;
     for (i = 0; i < A->nrows; i++) {
       if (A->deg[i] > 0)
         for (m = 0; m < A->deg[i]; m++) {
-          sum += x[A->adj[i][m]] * A->value[i][l];
+          sum += v[j * A->nrows + A->adj[i][m]] * A->value[i][l];
           l++;
         }
       // WRITE to y[i]
-      y[i] = sum;
+      v[(j + 1) * A->nrows + i] = sum;
       sum = 0.0;
       l = 0;
     }
 
     if (j > 0) {
-      x = v + A->nrows * (j - 1);
-      y = v + A->nrows * (j + 1);
       for (i = 0; i < A->nrows; i++) {
         // WRITE to y[i]
         // READ from arrt[j-1][j]
-        y[i] += (-arrt[(j - 1) * (k + 1) + j]) * x[i];
+        v[(j + 1) * A->nrows + i] += (-arrt[(j - 1) * (k + 1) + j]) * v[(j - 1) * A->nrows + i];
       }
     }
     sum = 0.0;
-    x = v + A->nrows * j;
-    y = v + A->nrows * (j + 1);
     for (i = 0; i < A->nrows; i++) {
       // READ from y[i]
-      sum += x[i] * y[i];
+      sum += v[j * A->nrows + i] * v[(j + 1) * A->nrows + i];
     }
     // WRITE to arrt[j][j]
     arrt[j * (k + 1) + j] = sum;
 
-    x = v + A->nrows * j;
-    y = v + A->nrows * (j + 1);
     for (i = 0; i < A->nrows; i++) {
       // WRITE to y[i]
       // READ from arrt[j][j]
-      y[i] += (-arrt[j * (k + 1) + j]) * x[i];
+      v[(j + 1) * A->nrows + i] += (-arrt[j * (k + 1) + j]) * v[j * A->nrows + i];
     }
     sum = 0.0;
-    x = v + A->nrows * (j + 1);
     for (i = 0; i < A->nrows; i++) {
-      sum += x[i] * x[i];
+      sum += v[(j + 1) * A->nrows + i] * v[(j + 1) * A->nrows + i];
     }
     // WRITE to arrt[j+1][j]
     arrt[(j + 1) * (k + 1) + j] = sqrt(sum);
-    x = v + A->nrows * (j + 1);
     for (i = 0; i < A->nrows; i++) {
       // READ from arrt[j+1][j]
-      x[i] *= 1 / arrt[(j + 1) * (k + 1) + j];
+      v[(j + 1) * A->nrows + i] *= 1 / arrt[(j + 1) * (k + 1) + j];
     }
     // WRITE to arrt[j][j+1]
     // READ from arrt[j+1][j]
